8ababb11607e8a8b0eda109ca24c60b48aaad99c
DevEmmanuel331 <alhassanemmanuel56@gmail.com>
2024-12-08 19:14:53 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const musicList = [
+        { title:'AMERADO - I AM AWARE MP3 DOWNLOAD ', link: 'aware.html' },
         { title:'DAVIDO - FUNDS FT ODUMODUBLVCK, CHIKE MP3 DOWNLOAD ', link: 'funds.html' },
         { title:'PRINCESS - JOEY B MP3 DOWNLOAD ', link: 'princess.html' },
         { title:'LIL BABA- NAZEL 3LA FO2   MP3 DOWNLOAD ', link: '3la.html' },
