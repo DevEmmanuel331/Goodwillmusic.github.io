965ab05a55b1db24b6e3c994dfff0524569584e7
DevEmmanuel331 <alhassanemmanuel56@gmail.com>
2024-12-20 20:22:15 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 document.addEventListener('DOMContentLoaded', () => {
     const musicList = [
 
+         { title:'PIESIE ESTHER - ATƐƐNE (OVERTURNED) MP3 DOWNLOAD ', link: 'over.html' },
 
          { title:'PIESIE ESTHER - ATƐƐNE (OVERTURNED) MP3 DOWNLOAD ', link: 'over.html' },
          { title:'MR DREW - SUMƆ MI FT MEDIKAL MP3 DOWNLOAD ', link: 'sumi.html' },
@@ -176,4 +177,23 @@ const handleSubmit = event => {
     });
   };
 
-  
+ <script>
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}    
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+  setTimeout(showSlides, 2000); // Change image every 2 seconds
+} 
