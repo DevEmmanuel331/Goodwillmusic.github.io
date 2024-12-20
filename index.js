ae52c71d872104a2da223b6e21250a0403323b82
DevEmmanuel331 <alhassanemmanuel56@gmail.com>
2024-12-20 21:11:32 +0000

    Update index.js

--- index.js
+++ index.js
@@ -196,3 +196,22 @@ function showSlides() {
   dots[slideIndex-1].className += " active";
   setTimeout(showSlides, 8000); // Change image every 2 seconds
 } 
+let slideIndex = [1,1];
+let slideId = ["mySlides1", "mySlides2"]
+showSlides(1, 0);
+showSlides(1, 1);
+
+function plusSlides(n, no) {
+  showSlides(slideIndex[no] += n, no);
+}
+
+function showSlides(n, no) {
+  let i;
+  let x = document.getElementsByClassName(slideId[no]);
+  if (n > x.length) {slideIndex[no] = 1}    
+  if (n < 1) {slideIndex[no] = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";  
+  }
+  x[slideIndex[no]-1].style.display = "block";  
+}
