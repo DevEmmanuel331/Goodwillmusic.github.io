1f61c3c9dc9b080c2d8dd62d167d82e0e2381e67
DevEmmanuel331 <alhassanemmanuel56@gmail.com>
2024-12-21 21:40:04 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const musicList = [
-
+        { title:'SZA - DRIVE MP3 DOWNLOAD ', link: 'drive.html' },
          { title:'SZA - GET BEHIND ME (INTERLUDE) MP3 DOWNLOAD ', link: 'Behind.html' },
          { title:'PIESIE ESTHER - ATƐƐNE (OVERTURNED) MP3 DOWNLOAD ', link: 'over.html' },
          { title:'MR DREW - SUMƆ MI FT MEDIKAL MP3 DOWNLOAD ', link: 'sumi.html' },
@@ -177,22 +177,4 @@ const handleSubmit = event => {
   };
 
  
-let slideIndex = 0;
-showSlides();
 
-function showSlides() {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";  
-  }
-  slideIndex++;
-  if (slideIndex > slides.length) {slideIndex = 1}    
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";  
-  dots[slideIndex-1].className += " active";
-  setTimeout(showSlides, 8000); // Change image every 2 seconds
-} 
