e8a00b721f275736ed6da91c0c9f74fb95e6ad02
DevEmmanuel331 <alhassanemmanuel56@gmail.com>
2024-12-28 05:57:37 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const musicList = [
+        { title:'MUZIC - AWAY FT YSMANRASTA MP3 DOWNLOAD ', link: 'away1.html' },
          { title:'SAIDA BOJ - GOLD DIGGER MP3 DOWNLOAD ', link: 'gold1.html' },
          { title:'MAVIS ASANTE - ASEDA NNWOM MP3 DOWNLOAD ', link: 'aseda1.html' },
          { title:'TOO LATE BY WENDY SHAY MP3 DOWNLOAD ', link: 'toolate.html' },
