dda00595a4dc4603ec290bd2d3070019dc9fd7ad
DevEmmanuel331 <alhassanemmanuel56@gmail.com>
2024-12-29 10:03:28 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const musicList = [
-                { title:'KING PALUTA FT SHATTA WALE - ADIEPENA MP3 DOWNLOAD ', link: 'King_Paluta_ft_Shatta_Wale_Adiepena.html' },
+                { title:'DJ SLIM FT KING PALUTA - BRONYA MP3 DOWNLOAD ', link: 'Dj_Slim___King_Paluta_-_BRONYA.html' },
         { title:'KING PALUTA FT SHATTA WALE - ADIEPENA MP3 DOWNLOAD ', link: 'King_Paluta_ft_Shatta_Wale_Adiepena.html' },
         { title:'2 PHÚT HƠN FUNK MP3 DOWNLOAD ', link: '2_PHÚT_HƠN_FUNK.html' },
         { title:'MUZIC - AWAY FT YSMANRASTA MP3 DOWNLOAD ', link: 'away1.html' },
