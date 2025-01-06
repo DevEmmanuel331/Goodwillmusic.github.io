a4131d783538cc08598acb8f7ffa222c2e49098a
DevEmmanuel331 <alhassanemmanuel56@gmail.com>
2025-01-06 19:55:17 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
     const musicList = [
+        { title:'SARKODIE - MESSIAH FT KWEKU FLICK MP3 DOWNLOAD ', link: 'Sarkodie_-_Messiah_ft._Kweku_Flick.html' },
         { title:'SZA - 30 FOR 30 FEAT. KENDRICK LAMAR MP3 DOWNLOAD ', link: 'SZA_-_30_For_30_feat._Kendrick_Lamar.html' },
         { title:'BLACK SHERIF - SHUT UP MP3 DOWNLOAD ', link: 'Black_Sherif_-_Shut_Up.html' },
         { title:'BLACK SHERIF - KILOS MILOS MP3 DOWNLOAD ', link: 'Black_Sherif_-_Kilos_Milos.html' },
